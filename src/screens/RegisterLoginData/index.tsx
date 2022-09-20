d79e3883f1058267ea727c278c587f42ff44c0e7
Rodolfo Viana <rodolfoviana@Rodolfos-MacBook-Pro.local>
2022-09-19 21:28:24 -0300

    Implementação do desafio 01

--- src/screens/RegisterLoginData/index.tsx
+++ src/screens/RegisterLoginData/index.tsx
@@ -58,9 +58,21 @@ export function RegisterLoginData() {
     const dataKey = '@savepass:logins';
 
     // Save data on AsyncStorage and navigate to 'Home' screen
+
+    const response = await AsyncStorage.getItem(dataKey);
+    const parsedData = JSON.parse(response) || [];
+
+    const newLoginDataList = [
+      ...parsedData,
+      newLoginData
+    ]
+
+    await AsyncStorage.setItem(dataKey, JSON.stringify(newLoginDataList));
+
+    navigate('Home');
   }
 
-  return (
+  return (  
     <KeyboardAvoidingView
       style={{ flex: 1 }}
       behavior={Platform.OS === 'ios' ? 'padding' : undefined}
@@ -73,10 +85,7 @@ export function RegisterLoginData() {
             testID="service-name-input"
             title="Nome do serviço"
             name="service_name"
-            error={
-              // Replace here with real content
-              'Has error ? show error message'
-            }
+            error={errors.service_name && errors.service_name.message}
             control={control}
             autoCapitalize="sentences"
             autoCorrect
@@ -85,10 +94,7 @@ export function RegisterLoginData() {
             testID="email-input"
             title="E-mail ou usuário"
             name="email"
-            error={
-              // Replace here with real content
-              'Has error ? show error message'
-            }
+            error={errors.email && errors.email.message}
             control={control}
             autoCorrect={false}
             autoCapitalize="none"
@@ -98,10 +104,7 @@ export function RegisterLoginData() {
             testID="password-input"
             title="Senha"
             name="password"
-            error={
-              // Replace here with real content
-              'Has error ? show error message'
-            }
+            error={errors.password && errors.password.message}
             control={control}
             secureTextEntry
           />
